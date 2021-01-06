1fee956d57502321db8db8125430ec9088263e0a
luanacesar <luana_cesar@hotmail.com>
2021-01-06 15:02:36 -0500

    third section is done

--- SlotMachine/Machine.js
+++ SlotMachine/Machine.js
@@ -9,14 +9,14 @@ class Machine extends React.Component{
     render(){
         let props = this.props;
         let msg;
-        const estilo = {fontSize: '50px', backgroundColor:'purple', width:'50%', textAlign: 'right',padding:'10px'};
+        const estilo = {fontSize: '50px', backgroundColor:'purple'};
 
         if(this.props.s1 == props.s2 && props.s2 == props.s3)
         {
-            msg = <h2>YOU WIN!</h2>  
+            msg = <h2 className={'Win'}>YOU WIN!</h2>  
         } else 
         {
-                msg = <h2>you lose!</h2>
+                msg = <h2 className={'lose'}>you lose!</h2>
         }
         return(
             <div className="Machine">
